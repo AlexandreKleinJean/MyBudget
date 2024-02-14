103e18182bcab9731f74d395306a43b0a247c418
Alexandre Klein <alexandreklein@AirdeAlexandre.lan>
2024-02-14 13:08:21 +0100

    register and login validation ok

--- src/main/java/com/myBudget/myBudget/controllers/AuthController.java
+++ src/main/java/com/myBudget/myBudget/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.myBudget.myBudget.controllers;
 import com.myBudget.myBudget.models.Client;
 import com.myBudget.myBudget.repositories.ClientRepository;
 import com.myBudget.myBudget.security.JwtUtil;
+import com.myBudget.myBudget.services.RegisterValidationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,25 +14,49 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RestController
 public class AuthController {
 
-    // J'implémente le ClientRepository pour accéder aux methods CRUD 
+    // J'injecte le ClientRepository pour accéder aux methods CRUD 
     @Autowired
     private ClientRepository clientRepository;
 
-    // J'implémente le PasswordEncord pour accéder à la method de hachage
+    // J'injecte le PasswordEncord pour accéder à la method de hachage
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    // J'injecte mon fichier pour valider les données d'inscription
+    @Autowired
+    private RegisterValidationService registerValidationService;
+
+
     /*-----------------S'inscrire (Signin)-----------------*/
     @PostMapping("/register")
-    public ResponseEntity<Client> signUp(@RequestBody Client newClient) {
+    public ResponseEntity<?> signUp(@RequestBody Client newClient) {
+
+        // Je check que l'email n'existe pas déjà
+        Client client = clientRepository.findByEmail(newClient.getEmail());
+
+        if (client != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+            .body("Email already used");
+        } 
+
+        // Je check que les données reçues sont au format correct
+        String validationError = registerValidationService.registrationValidation(
+            newClient.getGender(),
+            newClient.getFirstname(),
+            newClient.getLastname(),
+            newClient.getEmail(),
+            newClient.getPassword()
+        );
+
+        if (validationError != null) {
+            return ResponseEntity.badRequest().body(validationError);
+        }
         
         // Je remplace le password brut du user par son password hachée
         newClient.setPassword(passwordEncoder.encode(newClient.getPassword()));
         
         // J'enregistre le nouveau user en BDD
         Client savedClient = clientRepository.save(newClient);
-        
-        //************** GERER LA VALIDATION DE CHAMPS ***************/
 
         // J'envoi un code 201 + les infos du nouveau user
         return ResponseEntity.status(HttpStatus.CREATED).body(savedClient);
@@ -44,33 +69,26 @@ public class AuthController {
         // Je cherche un client qui a l'email correspondant
         Client client = clientRepository.findByEmail(loggedClient.getEmail());
 
-        // Le client existe
-        if (client != null) {
-
-            // Si password entré = password haché stocké en BDD
-            if (passwordEncoder.matches(loggedClient.getPassword(), client.getPassword())) {
-                
-                // Génération du JWT avec l'e-mail du client
-                String jwtToken = JwtUtil.generateJwtToken(client.getEmail());
-
-                System.out.println("JWT généré : " + jwtToken);
-
-                // Je retourne la reponse => Jwt unique + infos du client
-                return ResponseEntity.ok()
-                    .header("Authorization", "Bearer " + jwtToken)
-                    .body(client);
-            
-            // Le password ne correspond pas
-            } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body("Password is not correct");
-            }
-
-        // Le client n'existe pas
-        } else {
+        // Si email n'existe pas
+        if (client == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body("No user with this email");
-        }
+        } 
+
+        // Si password entré est différent de password haché stocké en BDD
+        if (!passwordEncoder.matches(loggedClient.getPassword(), client.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+            .body("Password is not correct");
+        }  
+                
+        // Génération du JWT avec l'e-mail du client
+        String jwtToken = JwtUtil.generateJwtToken(client.getEmail());
+        System.out.println("JWT généré : " + jwtToken);
+
+        // Je retourne la reponse => Jwt unique + infos du client
+        return ResponseEntity.ok()
+            .header("Authorization", "Bearer " + jwtToken)
+            .body(client);
     }
 }
 
---
+++ src/main/java/com/myBudget/myBudget/services/RegisterValidationService.java
@@ -0,0 +1,33 @@
+package com.myBudget.myBudget.services;
+
+import org.springframework.stereotype.Service;
+import org.apache.commons.validator.routines.EmailValidator;
+
+@Service
+public class RegisterValidationService {
+
+    public String registrationValidation(
+        String gender,
+        String firstname,
+        String lastname,
+        String email,
+        String password
+    ) {
+        if (gender == ""){
+            return "Gender has to be Male, Female or Other";
+        }
+        if (firstname == "" || !firstname.matches("^[a-zA-Z ]+$")) {
+            return "Firstname has to contain only letters and spaces";
+        }
+        if (lastname == "" || !lastname.matches("^[a-zA-Z ]+$")) {
+            return "Lastname has to contain only letters and spaces";
+        }
+        if (email == "" || !EmailValidator.getInstance().isValid(email)) {
+            return "Email format is not correct";
+        }
+        if (password == "") {
+            return "Password is mandatory";
+        }
+        return null;
+    }
+}
